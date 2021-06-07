324d32da0302eed83e8479fc1e09af69b5677f26
carlosworld <carloswolrd93@gmail.com>
2021-06-07 16:44:54 -0500

    actualizacion

--- components/common/Head/Head.tsx
+++ components/common/Head/Head.tsx
@@ -28,7 +28,6 @@ const Head: FC = () => {
           rel="stylesheet"
           href="https://pro.fontawesome.com/releases/v5.10.0/css/all.css"
           integrity="sha384-AYmEC3Yw5cVb3ZcuHtOA93w35dYTsvhLPVnYs9eStHfGJvOvKxVfELGroGkvsg+p"
-          crossorigin="anonymous"
         />
       </NextHead>
     </>
